dad880ed2efd6084062367c4ea6a2f731c21eb72
sowmik-sec <sowmik.sec@gmail.com>
2023-03-23 10:16:14 +0600

    featured review section added

--- src/pages/Home/Home/Home.js
+++ src/pages/Home/Home/Home.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Banner from "../Banner/Banner";
+import FeaturedReview from "../FeaturedReview/FeaturedReview";
 import FeaturedServices from "../FeaturedServices/FeaturedServices";
 
 const Home = () => {
@@ -7,6 +8,7 @@ const Home = () => {
     <div>
       <Banner />
       <FeaturedServices />
+      <FeaturedReview />
     </div>
   );
 };
