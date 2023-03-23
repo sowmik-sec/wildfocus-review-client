da20e8050c6d0a8f2b006a0836ae0691e57778f8
sowmik-sec <sowmik.sec@gmail.com>
2023-03-23 11:30:57 +0600

    services display updated

--- src/pages/Home/FeaturedServices/Service.js
+++ src/pages/Home/FeaturedServices/Service.js
@@ -1,15 +1,24 @@
 import React from "react";
 
 const Service = ({ service }) => {
-  const { name, title, img, description } = service;
+  const { instructorName, serviceImg, title, instructorImage, description } =
+    service;
   return (
     <div className="card w-96 bg-base-100 shadow-xl mx-auto">
       <figure>
-        <img src={img} alt="photographer" />
+        <img src={serviceImg} alt="photography" className="h-60 w-full" />
       </figure>
       <div className="card-body">
-        <h2 className="card-title">{name}</h2>
-        <p>{title}</p>
+        <h2 className="card-title">{title}</h2>
+        <p>{description.slice(0, 50) + "..."}</p>
+        <div className="flex items-center">
+          <img
+            src={instructorImage}
+            alt=""
+            className="w-14 h-14 rounded-full"
+          />
+          <p className="ml-2">{instructorName}</p>
+        </div>
       </div>
     </div>
   );
